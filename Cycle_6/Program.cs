67555d2830f1cec7168993c5ae380a15e8d35814
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-05 01:16:11 +0200

    Добавьте файлы проекта.

---
+++ Cycle_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите десятичное число: ");
+        int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Переведення в двійкову систему числення
+        string binaryRepresentation = DecimalToBinary(decimalNumber);
+
+        // Виведення результату
+        Console.WriteLine($"Двоичный вид числа {decimalNumber}: {binaryRepresentation}");
+    }
+
+    static string DecimalToBinary(int number)
+    {
+        if (number == 0)
+        {
+            return "0";
+        }
+
+        string binary = "";
+
+        while (number > 0)
+        {
+            int remainder = number % 2;
+            binary = remainder + binary;
+            number /= 2;
+        }
+
+        return binary;
+    }
+}
